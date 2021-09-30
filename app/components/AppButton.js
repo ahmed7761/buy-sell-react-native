abf4234f30abfd96361283dc539e7abf68f9f9cf
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-30 14:21:27 +0600

    homepage button and title design

--- app/components/AppButton.js
+++ app/components/AppButton.js
@@ -4,9 +4,9 @@ import {TouchableOpacity, Text, StyleSheet} from "react-native";
 import colors from "../config/colors";
 
 
-const AppButton = ({title, onPress}) => {
+const AppButton = ({title, onPress, color = "primary"}) => {
     return (
-        <TouchableOpacity style={styles.button} onPress={onPress} >
+        <TouchableOpacity style={[styles.button, {backgroundColor: colors[color]}]} onPress={onPress} >
             <Text style={styles.text}>{title}</Text>
         </TouchableOpacity>
     );
@@ -19,7 +19,8 @@ const styles = StyleSheet.create({
        justifyContent: 'center',
        alignItems: 'center',
        padding: 15,
-       width: '100%'
+       width: '100%',
+       marginVertical: 10
    },
    text: {
        color: colors.white,
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -7,15 +7,19 @@ import AppButton from "../components/AppButton";
 const WelcomeScreen = () => {
     return (
         <ImageBackground
+            blurRadius={2}
             style={styles.background}
             source={require('../assets/background.jpg')}
         >
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require('../assets/logo-red.png')} />
-                <Text>Sell What You Don't Need</Text>
+                <Text style={styles.tagline}>Sell What You Don't Need</Text>
             </View>
-            <AppButton title="Login" onPress={() => alert('hi')} />
-            <View style={styles.registerButton}/>
+            <View style={styles.buttonContainer}>
+                <AppButton title="Login"  onPress={() => alert('hi')} />
+                <AppButton title="Register" color="secondary" onPress={() => alert('hi')} />
+            </View>
+
         </ImageBackground>
 
     );
@@ -27,15 +31,9 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-end',
         alignItems: 'center'
     },
-    loginButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: colors.primary
-    },
-    registerButton: {
-        width: '100%',
-        height: 70,
-        backgroundColor: colors.secondary
+    buttonContainer:{
+        padding: 20,
+        width: '100%'
     },
     logo: {
         width: 100,
@@ -45,6 +43,12 @@ const styles = StyleSheet.create({
         position: 'absolute',
         top: 70,
         alignItems: 'center'
+    },
+    tagline: {
+        fontSize: 25,
+        fontWeight: "600",
+        paddingVertical: 20,
+        color: colors.black,
     },
 });
 
