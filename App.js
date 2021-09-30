f5e7530cd66b1ebe90bb18c9d639c5c051126423
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-30 12:44:44 +0600

    view image page design

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, Platform, StatusBar, TouchableOpacity, Alert, TouchableWithoutFeedback } from 'react-native';
 import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ViewImageScreen from "./app/screens/ViewImageScreen";
 
 
 export default function App() {
   return (
-    <WelcomeScreen/>
+    // <WelcomeScreen/>
+      <ViewImageScreen/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    flexDirection: "column",
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-  },
-});
