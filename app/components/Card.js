6ec2f7f327ef493e600e8da7a70aef78f053f2bd
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-30 16:08:05 +0600

    card design

---
+++ app/components/Card.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {ImageBackground, StyleSheet, View, Image, Text} from "react-native";
+import colors from "../config/colors";
+
+const Card = ({ title, subTitle, image }) => {
+    return (
+        <View style={styles.card}>
+            <Image style={styles.image} source={image}/>
+            <View style={styles.detailsContainer}>
+                <Text style={styles.title}>{ title }</Text>
+                <Text style={styles.subTitle}>{ subTitle }</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 15,
+        backgroundColor: colors.white,
+        marginBottom: 20,
+        overflow: "hidden"
+    },
+    detailsContainer: {
+        padding: 20
+    },
+    image: {
+        width: "100%",
+        height: 200
+    },
+    subTitle: {
+        color: colors.secondary,
+        fontWeight: "bold"
+    },
+    title: {
+        marginBottom: 20
+    }
+});
+
+export default Card;
