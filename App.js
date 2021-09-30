7531a251a99d643af34a6ff7d391fa2233befdb1
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-09-30 16:41:12 +0600

    designed list details screen

--- App.js
+++ App.js
@@ -3,22 +3,24 @@ import { StyleSheet, Text, View, Image, Platform, StatusBar, TouchableOpacity, A
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import Card from "./app/components/Card";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 
 export default function App() {
   return (
      // <WelcomeScreen/>
     //   <ViewImageScreen/>
-      <View style={{
-        backgroundColor: '#f8f4f4',
-        padding: 20,
-        paddingTop: 100
-      }}>
-        <Card
-          title="Red jacket for sale"
-          subTitle="$100"
-          image={require("./app/assets/jacket.jpg")}
-        />
-      </View>
+    //   <View style={{
+    //     backgroundColor: '#f8f4f4',
+    //     padding: 20,
+    //     paddingTop: 100
+    //   }}>
+    //     <Card
+    //       title="Red jacket for sale"
+    //       subTitle="$100"
+    //       image={require("./app/assets/jacket.jpg")}
+    //     />
+    //   </View>
+      <ListingDetailsScreen />
   );
 }
