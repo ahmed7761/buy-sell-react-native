2763a425f511e5a87519404ac0d7f238165b9e3c
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-10-03 16:52:16 +0600

    updated listing edit screen design

--- app/screens/ListingEditScreen.js
+++ app/screens/ListingEditScreen.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import * as Yup from 'yup';
+import React from "react";
+import { StyleSheet } from "react-native";
+import * as Yup from "yup";
 
+import {
+    AppForm as Form,
+    AppFormField as FormField,
+    AppFormPicker as Picker,
+    SubmitButton,
+} from "../components/forms";
 import Screen from "../components/Screen";
-import {AppFormField, SubmitButton, AppForm, AppFormPicker} from '../components/forms'
 
 const validationSchema = Yup.object().shape({
     title: Yup.string().required().min(1).label("Title"),
@@ -13,55 +18,48 @@ const validationSchema = Yup.object().shape({
 });
 
 const categories = [
-    {label: 'Furniture', value: 1},
-    {label: 'Clothing', value: 2},
-    {label: 'Camera', value: 3},
+    { label: "Furniture", value: 1 },
+    { label: "Clothing", value: 2 },
+    { label: "Camera", value: 3 },
 ];
 
-const ListingEditScreen = () => {
+function ListingEditScreen() {
     return (
         <Screen style={styles.container}>
-            <AppForm
+            <Form
                 initialValues={{
                     title: "",
                     price: "",
                     description: "",
                     category: null,
                 }}
-                onSubmit={(values)=> console.log(values)}
+                onSubmit={(values) => console.log(values)}
                 validationSchema={validationSchema}
             >
-                <AppFormField
-                    maxLength={255}
-                    name='title'
-                    placeholder='Title'
-                />
-                <AppFormField
-                    maxLength="numeric"
+                <FormField maxLength={255} name="title" placeholder="Title" />
+                <FormField
+                    keyboardType="numeric"
                     maxLength={8}
-                    name='price'
-                    placeholder='Price'
+                    name="price"
+                    placeholder="Price"
                 />
-                <AppFormField
-                    items={categories}
-                    name='category'
-                    placeholder='Category'
-                />
-                <AppFormField
+                <Picker items={categories} name="category" placeholder="Category" />
+                <FormField
                     maxLength={255}
                     multiline
-                    name='description'
-                    placeholder='Description'
+                    name="description"
+                    numberOfLines={3}
+                    placeholder="Description"
                 />
-            </AppForm>
+                <SubmitButton title="Post" />
+            </Form>
         </Screen>
     );
-};
+}
 
 const styles = StyleSheet.create({
     container: {
         padding: 10,
     },
 });
-
 export default ListingEditScreen;
