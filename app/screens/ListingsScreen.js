e3291e9f11ec3e9809213b2b68b8f4c9780e0047
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-10-06 18:12:06 +0600

    caching images

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -36,6 +36,7 @@ const ListingsScreen = ({ navigation }) => {
                         subTitle={"$" + item.price}
                         imageUrl={item.images[0].url}
                         onPress={() => navigation.navigate("ListingDetails", item)}
+                        thumbnailUrl={item.images[0].thumbnailUrl}
                     />
                 }
             />
