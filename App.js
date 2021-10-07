dc7dbb28e16167ec9458b01b719bdc62211b6798
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-10-07 13:32:22 +0600

    updated splash screen

--- App.js
+++ App.js
@@ -30,20 +30,27 @@ import OfflineNotice from "./app/components/OfflineNotice";
 import AuthContext from "./app/auth/context";
 import authStorage from "./app/auth/storage";
 import jwtDecode from "jwt-decode";
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
   const [user, setUser] = useState();
+  const [isReady, setIsReady] = useState(false);
   const restoreToken = async () => {
-      const token = await authStorage.getToken()
+      const token = await authStorage.getToken();
       if(!token) return;
       setUser(jwtDecode(token));
   };
 
-  useEffect(() =>{
-      restoreToken()
-  },[]);
+    if(!isReady)
+    return (
+        <AppLoading
+            startAsync={restoreToken}
+            onFinish={() => setIsReady(true)}
+            onError={console.warn}
+        />
+    );
 
-  return (
+    return (
         <AuthContext.Provider value={{ user, setUser }} >
             <NavigationContainer theme={navigationTheme} >
                 { user ?  <AppNavigator /> : <AuthNavigator/>}
