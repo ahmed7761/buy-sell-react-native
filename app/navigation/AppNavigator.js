3626a310c3142cc62aa3065a79a61f75d164945f
ahmed7761 <76873374+ahmed7761@users.noreply.github.com>
2021-10-07 18:13:44 +0600

    expo push token

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
@@ -7,38 +7,55 @@ import FeedNavigator from "./FeedNavigator";
 import AccountNavigator from "./AccountNavigator";
 import NewListingButton from "./NewListingButton";
 import routes from "./routes";
+import * as Notifications from 'expo-notifications';
 
 const Tab = createBottomTabNavigator();
 
-const AppNavigator = () => (
-    <Tab.Navigator>
-        <Tab.Screen
-            name={routes.FEED}
-            component={FeedNavigator}
-            options={{
-                tabBarIcon: ({ color, size }) =>
-                <MaterialCommunityIcons name='home' color={color} size={size} />
-            }}
-        />
-        <Tab.Screen
-            name={routes.LISTING_EDIT}
-            component={ListingEditScreen}
-            options={({ navigation }) =>  ({
+const AppNavigator = () => {
+
+    useEffect(() => {
+        registerForPushNotifications();
+    },[]);
+
+    const registerForPushNotifications =  async () => {
+        try {
+            const token = await Notifications.getExpoPushTokenAsync();
+            console.log(token);
+        } catch (e) {
+            console.log("error", e)
+        }
+    };
+
+    return (
+        <Tab.Navigator>
+            <Tab.Screen
+                name={routes.FEED}
+                component={FeedNavigator}
+                options={{
+                    tabBarIcon: ({color, size}) =>
+                        <MaterialCommunityIcons name='home' color={color} size={size}/>
+                }}
+            />
+            <Tab.Screen
+                name={routes.LISTING_EDIT}
+                component={ListingEditScreen}
+                options={({navigation}) => ({
                     tabBarButton: () =>
-                        <NewListingButton onPress={() => navigation.navigate(routes.LISTING_EDIT)} />,
-                    tabBarIcon: ({ color, size }) =>
-                        <MaterialCommunityIcons name='plus-circle' color={color} size={size} />
-            })}
-        />
-        <Tab.Screen
-            name={routes.ACCOUNT}
-            component={AccountNavigator}
-            options={{
-                    tabBarIcon: ({ color, size }) =>
-                        <MaterialCommunityIcons name='account' color={color} size={size} />
-            }}
-        />
-    </Tab.Navigator>
-);
+                        <NewListingButton onPress={() => navigation.navigate(routes.LISTING_EDIT)}/>,
+                    tabBarIcon: ({color, size}) =>
+                        <MaterialCommunityIcons name='plus-circle' color={color} size={size}/>
+                })}
+            />
+            <Tab.Screen
+                name={routes.ACCOUNT}
+                component={AccountNavigator}
+                options={{
+                    tabBarIcon: ({color, size}) =>
+                        <MaterialCommunityIcons name='account' color={color} size={size}/>
+                }}
+            />
+        </Tab.Navigator>
+    )
+};
 
 export default AppNavigator;
